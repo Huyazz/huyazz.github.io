eb3218e103a1d20914f86488883d7e323c467226
Huyazz <115351102+Huyazz@users.noreply.github.com>
2023-12-28 21:36:02 +0700

    Add files via upload

---
+++ python (1).py
@@ -0,0 +1,8 @@
+a = int(input('nhập mật khẩu để giải mã thông điệp gồm 7 chữ số :'))
+# không cần gán c
+c= a
+if c > 112006 and c < 1102008:
+    print("I love you ")
+else:
+    print("saii!")
+    
